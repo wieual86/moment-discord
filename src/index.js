053726d8b7cb394de0b595b079099d712c670dcb
EA <narrativeengineers@gmail.com>
2020-08-15 20:45:28 -0400

    Fixed it so e works.

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ const getParams = message => {
     const diceMatch = text.match(/.*\.(?:roll|r)([^\w!]*\d+(?:[^\w!]+\d+)*[^\w!]*)/);
     if (!diceMatch) return null;
 
-    const expertiseMatch = text.match(/.*\.(?:roll|r).*(expertise|!)/);
+    const expertiseMatch = text.match(/.*\.(?:roll|r).*(expertise|[^v]e|!)/);
     const burstMatch = text.match(/.*\.(?:roll|r).*(burst|b)/);
     const initiativeMatch = text.match(
       /.*\.(?:roll|r).*(?:initiative|i)([^\w!]*\d+(?:[^\w!]+\d+)*[^\w!]*)?/
